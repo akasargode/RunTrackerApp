220456d223ca199738c8aeb8124128b413a15edc
Ashwin Kasargode <akasargode@Ashwins-MacBook-Air-3.local>
2019-04-22 11:44:28 -0500

    Stopwatch Done

---
+++ RunTrackerApp/StopwatchViewController.swift
@@ -0,0 +1,54 @@
+//
+//  StopwatchViewController.swift
+//  FinalProject
+//
+//  Created by Ashwin Kasargode on 12/7/18.
+//  Copyright © 2018 Ashwin Kasargode. All rights reserved.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @objc func UpdateTimer() {
+        counter = counter + 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+    @IBAction func startTimer(_ sender: Any) {
+        if(isPlaying) {
+            return
+        }
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        timer.invalidate()
+        isPlaying = false
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    var counter = 0.0
+    var timer = Timer()
+    
+    var isPlaying = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timeLabel.text = String(counter)
+        pauseButton.isEnabled = false
+    }
+}
