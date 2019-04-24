27e0b8e498786323b5bff57e9fecbcd355bb3c35
Parker Wokwicz <pwokwicz@parkers-macbook-air.local>
2019-04-24 11:35:27 -0500

    Location Manager Function

--- RunTrackerApp/MapViewController.swift
+++ RunTrackerApp/MapViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
-    var selectedMapItem = MKMapItem()
+    let locationManager = CLLocationManager()
     var region = MKCoordinateRegion()
     var mapItems = [MKMapItem]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        mapView.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+        region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
     }
 }
